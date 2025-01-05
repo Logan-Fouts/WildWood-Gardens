f638ef6783c1898f9a18d0feed420ec1747727b2
Logan-Fouts <loganfouts@proton.me>
2025-01-04 20:39:28 -0500

    v2 with seperate blog

--- src/app/page.js
+++ src/app/page.js
@@ -20,24 +20,41 @@ export default function Home() {
         </div>
 
         <div className="container w-full mx-auto max-w-6xl sm:px-16 sm:mt-12 md:mt-48 md:pt-0 sm:pt-32 pt-12 sm:text-left text-center">
-          <BonsaiBlog />
+          <StoreInfo />
         </div>
       </div>
     </div>
   );
 }
 
-function BonsaiBlog() {
+function StoreInfo() {
   return (
     <div className="sm:py-4 md:py-16">
       <div className="relative z-10">
-        <H1 className="text-4xl md:text-5xl lg:text-6xl text-black">Bonsai</H1>
-        <H1 className="-mt-4 md:-mt-6 text-4xl md:text-5xl lg:text-6xl text-black">journal</H1>
+        <H1 className="text-4xl md:text-5xl lg:text-6xl text-black">Wild</H1>
+        <H1 className="-mt-4 md:-mt-6 text-4xl md:text-5xl lg:text-6xl text-black">Wood</H1>
+        <H1 className="-mt-4 md:-mt-6 text-4xl md:text-5xl lg:text-6xl text-black">Gardens</H1>
         <h3 className="text-black text-md font-extralight mt-4 max-w-md md:max-w-lg lg:max-w-xl">
-          <p className="inline text-black">Whats happening </p>here at WildWood Gardens?
         </h3>
       </div>
-      <Blog />
+      <div className="md:mt-52 mt-16 text-gray-700 text-left pb-8">
+        <h1 className="text-4xl text-black pb-4">About us</h1>
+        <p className="mb-4">
+          Welcome to Wild Wood Gardens, your local bonsai store dedicated to the art and beauty of bonsai trees. We offer a wide variety of bonsai trees, tools, and accessories to help you create and maintain your own miniature masterpieces.
+        </p>
+        <p className="mb-4">
+          Our journey began over 20 years ago with a passion for bonsai and a dream to share this ancient art form with our community. Over the years, we have grown from a small hobbyist shop to a full-fledged bonsai nursery, offering a diverse selection of bonsai trees and supplies.
+        </p>
+        <p className="mb-4">
+          At Wild Wood Gardens, we believe in the therapeutic and meditative benefits of bonsai cultivation. Our mission is to provide a tranquil space where enthusiasts of all levels can explore, learn, and grow their bonsai skills.
+        </p>
+        <p className="mb-4">
+          We host regular workshops and classes to help you learn the techniques and skills needed to care for your bonsai. Our knowledgeable staff is always here to assist you with any questions or needs you may have.
+        </p>
+        <p className="mb-4">
+          Visit us today to explore our collection and discover the perfect bonsai tree for your home or garden. Whether you are a beginner looking to start your bonsai journey or an experienced enthusiast seeking rare and unique specimens, we have something for everyone.
+        </p>
+      </div>
     </div>
   );
 }
