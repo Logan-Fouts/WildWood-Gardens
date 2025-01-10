85cdbf727b42a815c4acc80eab158e579de3a9f2
Logan-Fouts <loganfouts@proton.me>
2025-01-09 21:39:06 -0500

    main image upload working

--- src/app/admin/editors/blog-creater.jsx
+++ src/app/admin/editors/blog-creater.jsx
@@ -1,5 +1,4 @@
 "use client"
-import { Blog } from "@/app/blog/page";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { useState } from "react";
 import { CldUploadWidget } from "next-cloudinary";
